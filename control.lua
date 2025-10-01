f54a0b4a389e86e8a0ea9a45e6a004dc2a67982d
William C. Johnson <wcjohnson@oigroup.net>
2025-10-01 12:54:08 -0700

    refactor: New events, step 1

--- control.lua
+++ control.lua
@@ -2,9 +2,8 @@
 -- THINGS CONTROL PHASE
 --------------------------------------------------------------------------------
 
-local counters = require("lib.core.counters")
+local event = require("lib.core.event")
 local scheduler = require("lib.core.scheduler")
-local events = require("lib.core.events")
 local actual = require("lib.core.blueprint.actual")
 
 require("lib.core.debug-log")
@@ -37,35 +36,29 @@ if script.active_mods["gvv"] then require("__gvv__.gvv")() end
 -- MOD LIFECYCLE
 --------------------------------------------------------------------------------
 
-on_startup(counters.init, true)
-on_startup(scheduler.init, true)
-
-script.on_init(raise_init)
+event.bind("on_init", raise_init)
 on_init(function() raise_startup({}) end, true)
-script.on_load(raise_load)
-
-script.on_configuration_changed(raise_configuration_changed)
-script.on_event(defines.events.on_runtime_mod_setting_changed, function(event)
+event.bind("on_load", raise_load)
+event.bind("on_configuration_changed", raise_configuration_changed)
+event.bind(defines.events.on_runtime_mod_setting_changed, function(event)
 	update_mod_settings()
 	raise_mod_settings_changed()
 end)
-
-script.on_nth_tick(1, function(data) scheduler.tick(data) end)
 
 --------------------------------------------------------------------------------
 -- BLUEPRINTING
 --------------------------------------------------------------------------------
 
-script.on_event(defines.events.on_player_setup_blueprint, function(event)
-	local player = game.get_player(event.player_index)
+event.bind(defines.events.on_player_setup_blueprint, function(ev)
+	local player = game.get_player(ev.player_index)
 	if not player then return end
-	local bp = actual.get_actual_blueprint(player, event.record, event.stack)
+	local bp = actual.get_actual_blueprint(player, ev.record, ev.stack)
 	if not bp then return end
-	raise_blueprint_extract(event, player, bp)
+	raise_blueprint_extract(ev, player, bp)
 end)
 
-script.on_event(defines.events.on_pre_build, function(event)
-	local player = game.get_player(event.player_index)
+event.bind(defines.events.on_pre_build, function(ev)
+	local player = game.get_player(ev.player_index)
 	if not player then return end
 	-- Blueprint
 	if player.is_cursor_blueprint() then
@@ -75,7 +68,7 @@ script.on_event(defines.events.on_pre_build, function(event)
 			player.cursor_stack
 		)
 		if not bp then return end
-		return raise_blueprint_apply(player, bp, player.surface, event)
+		return raise_blueprint_apply(player, bp, player.surface, ev)
 	end
 	-- Item with entity place result
 	local stack = player.cursor_stack
@@ -83,7 +76,7 @@ script.on_event(defines.events.on_pre_build, function(event)
 		local entity_placed = stack.prototype.place_result
 		if entity_placed then
 			return raise_pre_build_entity(
-				event,
+				ev,
 				player,
 				entity_placed,
 				stack.quality,
@@ -97,7 +90,7 @@ script.on_event(defines.events.on_pre_build, function(event)
 		local entity_placed = cursor_ghost.name.place_result
 		if entity_placed then
 			return raise_pre_build_entity(
-				event,
+				ev,
 				player,
 				entity_placed,
 				cursor_ghost.quality --[[@as LuaQualityPrototype]],
@@ -111,27 +104,23 @@ end)
 -- CONSTRUCTION
 --------------------------------------------------------------------------------
 
----@param event AnyFactorioBuildEventData
-local function handle_generic_built(event)
-	local player = event.player_index and game.get_player(event.player_index)
-		or nil
-	local entity = event.entity
+---@param ev AnyFactorioBuildEventData
+local function handle_generic_built(ev)
+	local player = ev.player_index and game.get_player(ev.player_index) or nil
+	local entity = ev.entity
 	if entity.type == "entity-ghost" then
-		raise_built_ghost(event, entity, entity.tags, player)
+		raise_built_ghost(ev, entity, entity.tags, player)
 	else
-		raise_built_real(event, entity, event.tags, player)
+		raise_built_real(ev, entity, ev.tags, player)
 	end
 end
 
-script.on_event(defines.events.on_built_entity, handle_generic_built)
-script.on_event(defines.events.on_robot_built_entity, handle_generic_built)
-script.on_event(
-	defines.events.on_space_platform_built_entity,
-	handle_generic_built
-)
-script.on_event(defines.events.on_entity_cloned, raise_entity_cloned)
-script.on_event(defines.events.script_raised_built, handle_generic_built)
-script.on_event(defines.events.script_raised_revive, handle_generic_built)
+event.bind(defines.events.on_built_entity, handle_generic_built)
+event.bind(defines.events.on_robot_built_entity, handle_generic_built)
+event.bind(defines.events.on_space_platform_built_entity, handle_generic_built)
+event.bind(defines.events.on_entity_cloned, raise_entity_cloned)
+event.bind(defines.events.script_raised_built, handle_generic_built)
+event.bind(defines.events.script_raised_revive, handle_generic_built)
 
 --------------------------------------------------------------------------------
 -- DECONSTRUCTION AND DEATH
@@ -141,31 +130,24 @@ local function handle_pre_destroyed(event)
 	raise_unified_pre_destroy(event, event.entity, nil)
 end
 
-script.on_event(defines.events.on_pre_player_mined_item, function(event)
-	if event.player_index then
-		raise_unified_pre_destroy(
-			event,
-			event.entity,
-			game.get_player(event.player_index)
-		)
+event.bind(defines.events.on_pre_player_mined_item, function(ev)
+	if ev.player_index then
+		raise_unified_pre_destroy(ev, ev.entity, game.get_player(ev.player_index))
 	else
-		raise_unified_pre_destroy(event, event.entity, nil)
+		raise_unified_pre_destroy(ev, ev.entity, nil)
 	end
 end)
-script.on_event(defines.events.on_robot_pre_mined, handle_pre_destroyed)
-script.on_event(
-	defines.events.on_space_platform_pre_mined,
-	handle_pre_destroyed
-)
-script.on_event(defines.events.on_pre_ghost_deconstructed, function(event)
+event.bind(defines.events.on_robot_pre_mined, handle_pre_destroyed)
+event.bind(defines.events.on_space_platform_pre_mined, handle_pre_destroyed)
+event.bind(defines.events.on_pre_ghost_deconstructed, function(ev)
 	-- Ghost deconstruction is special as it doesn't fire a destroy event.
 	-- We synthesize it here.
-	if event.player_index then
-		local player = game.get_player(event.player_index)
-		raise_unified_pre_destroy(event, event.ghost, player)
-		raise_unified_destroy(event, event.ghost, player, true)
+	if ev.player_index then
+		local player = game.get_player(ev.player_index)
+		raise_unified_pre_destroy(ev, ev.ghost, player)
+		raise_unified_destroy(ev, ev.ghost, player, true)
 	else
-		raise_unified_destroy(event, event.ghost, nil, false)
+		raise_unified_destroy(ev, ev.ghost, nil, false)
 	end
 end)
 
@@ -173,71 +155,56 @@ local function handle_destroyed(event)
 	raise_unified_destroy(event, event.entity, nil, false)
 end
 
-script.on_event(defines.events.on_player_mined_entity, function(event)
-	if event.player_index then
-		raise_unified_destroy(
-			event,
-			event.entity,
-			game.get_player(event.player_index),
-			true
-		)
+event.bind(defines.events.on_player_mined_entity, function(ev)
+	if ev.player_index then
+		raise_unified_destroy(ev, ev.entity, game.get_player(ev.player_index), true)
 	else
-		raise_unified_destroy(event, event.entity, nil, false)
+		raise_unified_destroy(ev, ev.entity, nil, false)
 	end
 end)
-script.on_event(defines.events.on_robot_mined_entity, handle_destroyed)
-script.on_event(defines.events.on_space_platform_mined_entity, handle_destroyed)
-script.on_event(defines.events.script_raised_destroy, function(event)
+event.bind(defines.events.on_robot_mined_entity, handle_destroyed)
+event.bind(defines.events.on_space_platform_mined_entity, handle_destroyed)
+event.bind(defines.events.script_raised_destroy, function(ev)
 	-- Script destruction isn't undo-able
-	raise_unified_destroy(event, event.entity, nil, false)
+	raise_unified_destroy(ev, ev.entity, nil, false)
 end)
 
-script.on_event(defines.events.on_post_entity_died, raise_entity_died)
+event.bind(defines.events.on_post_entity_died, raise_entity_died)
 
 -- Marking and unmarking
-script.on_event(defines.events.on_marked_for_deconstruction, function(event)
-	if event.player_index then
-		raise_entity_marked(
-			event,
-			event.entity,
-			game.get_player(event.player_index)
-		)
+event.bind(defines.events.on_marked_for_deconstruction, function(ev)
+	if ev.player_index then
+		raise_entity_marked(ev, ev.entity, game.get_player(ev.player_index))
 	end
 end)
-script.on_event(defines.events.on_cancelled_deconstruction, function(event)
-	if event.player_index then
-		raise_entity_unmarked(
-			event,
-			event.entity,
-			game.get_player(event.player_index)
-		)
+event.bind(defines.events.on_cancelled_deconstruction, function(ev)
+	if ev.player_index then
+		raise_entity_unmarked(ev, ev.entity, game.get_player(ev.player_index))
 	end
 end)
 
 -- Undo/redo
 
-script.on_event(defines.events.on_undo_applied, raise_undo_applied)
-script.on_event(defines.events.on_redo_applied, raise_redo_applied)
+event.bind(defines.events.on_undo_applied, raise_undo_applied)
+event.bind(defines.events.on_redo_applied, raise_redo_applied)
 
 -- Orientation
 
-script.on_event(
+event.bind(
 	defines.events.on_player_flipped_entity,
-	function(event) raise_player_flipped_entity(event, event.entity) end
+	function(ev) raise_player_flipped_entity(ev, ev.entity) end
 )
 
-script.on_event(
+event.bind(
 	defines.events.on_player_rotated_entity,
-	function(event) raise_player_rotated_entity(event, event.entity) end
+	function(ev) raise_player_rotated_entity(ev, ev.entity) end
 )
 
 -- Settings
 
-script.on_event(
+event.bind(
 	defines.events.on_entity_settings_pasted,
-	function(event)
-		raise_entity_settings_pasted(event, event.source, event.destination)
-	end
+	function(ev) raise_entity_settings_pasted(ev, ev.source, ev.destination) end
 )
 
 -- API
