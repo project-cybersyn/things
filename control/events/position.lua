07af50b4bf1781820ed4f52cc427ed7805f1dfc5
William C. Johnson <wcjohnson@oigroup.net>
2026-07-11 00:29:56 -0700

    fix: unthing teleport delegation

--- control/events/position.lua
+++ control/events/position.lua
@@ -2,25 +2,43 @@ local events = require("lib.core.event")
 local thing_lib = require("control.thing")
 local pos_lib = require("lib.core.math.pos")
 
+local get_by_unit_number = thing_lib.get_by_unit_number
+local pos_new = pos_lib.pos_new
+local pos_add = pos_lib.pos_add
+
 events.bind(
 	defines.events.script_raised_teleported,
 	---@param ev EventData.script_raised_teleported
 	function(ev)
-		-- TODO: unthing children - if unthing child is moved, get root from parent Thing.
-		local thing = thing_lib.get_by_unit_number(ev.entity.unit_number)
-		if not thing then return end
+		local un = ev.entity.unit_number
+
+		-- Disentangle thing vs unthing
+		local thing = get_by_unit_number(un)
+		local root_thing
+		local is_different_root = true
+		if thing then
+			root_thing = thing:get_root()
+			is_different_root = root_thing.id ~= thing.id
+		else
+			local unthing = get_unthing_child(un)
+			if not unthing then return end
+			thing = get_by_unit_number(unthing[1])
+			if thing then
+				root_thing = thing:get_root()
+			else
+				return
+			end
+		end
 
 		local new_pos = ev.entity.position
-		local root_thing = thing:get_root()
-
-		if root_thing.id ~= thing.id then
+		if is_different_root then
 			-- Make sure the offset is preserved in the new frame of reference
 			local root_entity = root_thing:get_entity()
 			if not root_entity then return end
-			local offset = pos_lib.pos_new(ev.old_position)
-			pos_lib.pos_add(offset, -1, root_entity.position)
-			local new_root_pos = pos_lib.pos_new(new_pos)
-			pos_lib.pos_add(new_root_pos, -1, offset)
+			local offset = pos_new(ev.old_position)
+			pos_add(offset, -1, root_entity.position)
+			local new_root_pos = pos_new(new_pos)
+			pos_add(new_root_pos, -1, offset)
 
 			root_thing:teleport(new_root_pos)
 		else
